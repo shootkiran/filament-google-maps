e5fb2a3d8bffa81cc0b0e24b13cf8d38ba92c2f6
Gerardo Ibarra <ibarragp@gmail.com>
2024-10-20 19:22:13 -0300

    fix css, add parameter position, set default icon

--- resources/views/fields/filament-google-maps.blade.php
+++ resources/views/fields/filament-google-maps.blade.php
@@ -5,8 +5,9 @@
 
     <div
         x-ignore
-        x-load
-        x-load-src="{{ \Filament\Support\Facades\FilamentAsset::getAlpineComponentSrc('filament-google-maps-field', 'cheesegrits/filament-google-maps') }}"
+        ax-load
+        x-load-css="[@js(\Filament\Support\Facades\FilamentAsset::getStyleHref('filament-google-maps-field', 'cheesegrits/filament-google-maps'))]"
+        ax-load-src="{{ \Filament\Support\Facades\FilamentAsset::getAlpineComponentSrc('filament-google-maps-field', 'cheesegrits/filament-google-maps') }}"
         x-data="filamentGoogleMapsField({
                     state: $wire.entangle('{{ $getStatePath() }}'),
                     setStateUsing: (path, state) => {
@@ -26,6 +27,7 @@
                     geolocate: @js($getGeolocate()),
                     geolocateOnLoad: @js($getGeolocateOnLoad()),
                     geolocateLabel: @js($getGeolocateLabel()),
+                    geolocatePosition: @js($getGeolocatePosition()),
                     draggable: @js($getDraggable()),
                     clickable: @js($getClickable()),
                     defaultLocation: @js($getDefaultLocation()),
